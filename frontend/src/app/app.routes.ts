287673fe6e27e56e4a5004617c84a7de3fb5539f
patrick-ed <patrickedumdum@gmail.com>
2025-05-01 23:13:40 +0100

    added auth guard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { PinboardComponent } from './pages/pinboard/pinboard.component';
 import { UsersComponent } from './pages/users/users.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: AuthComponent },
-    { path: 'pinboard', component: PinboardComponent },
-    { path: 'users', component: UsersComponent }
+    { path: 'pinboard', component: PinboardComponent, canActivate: [authGuard] },
+    { path: 'users', component: UsersComponent },
+    { path: '**', redirectTo: '' },
 ];
